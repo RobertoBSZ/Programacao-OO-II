41c7914d0c60f9d8ce4f069a317ab259ab58d6a8
Roberto Bastos Souza <103335050+RobertoBSZ@users.noreply.github.com>
2023-03-06 16:32:18 -0300

    Add files via upload

--- contabancaria/main.cs
+++ contabancaria/main.cs
@@ -1,54 +1,63 @@
 /******************************************************************************
 -Aula dia 06/03-
-
 Criação de conta e função:
-
 Administrar uma conta bancária.
 O que vai precisar de Atributos na classe cliente?
-
 *CPF
 *Nome
 *Saldo Bancário
-
 Ações (Métodos)?
-
 *Sacar
 *Depositar
 *Extrato (Pode ser uma string)
-
-
-
 *******************************************************************************/
 using System;
 class HelloWorld {
   static void Main() {
+     
     Boolean opcao_menu = true;
-    Cliente cliente1 = new Cliente();
+    Conta cliente1 = new Conta();
     int operacaouser;
     
+    Console.WriteLine("Sistema Bancário\n");
+    Console.WriteLine("Criação de Conta\n");
+    Console.WriteLine("Cadastro de informações pessoais");
+    Console.WriteLine("\nDigite o seu CPF:");
+    cliente1.setCpf = Console.ReadLine();
+    
+    Console.WriteLine("\nDigite o seu nome:");
+    cliente1.setName = Console.ReadLine();
+    
+    Console.WriteLine("\nConfirmação de suas informações\n");
+    Console.WriteLine("CPF: " + cliente1.GetCpf());
+    Console.WriteLine("Nome: " + cliente1.GetName());
+    
+    Console.WriteLine("\nSeu saldo inicial é de R$1000,00\n");
+    
+    
     while(opcao_menu == true){
-        Console.WriteLine("Escolha sua operação bancária:\n1-Consultar\n2-Sacar\n3-Depositar\n4-Sair");
+        Console.WriteLine("\nEscolha sua operação bancária:\n1-Consultar\n2-Sacar\n3-Depositar\n4-Sair");
+        Console.WriteLine("\nSua escolha:");
         operacaouser = Convert.ToInt32(Console.ReadLine());
         switch(operacaouser){
             case 1:
-                Console.WriteLine("\nO seu saldo é:\n");
-                Console.WriteLine(cliente1.GetSaldo());
+                Console.WriteLine("\nSaldo atual: R$" + cliente1.GetSaldo());
                 break;
             case 2:
                 double x;
-                Console.WriteLine("\nQuanto você quer sacar?\n");
+                Console.WriteLine("\nQuanto você quer sacar?");
                 x = Convert.ToDouble(Console.ReadLine());
-                cliente1.sacar(x);
+                cliente1.Sacar(x);
                 
-                Console.WriteLine("Saldo atual: " + cliente1.GetSaldo());
+                Console.WriteLine("Saldo atual: R$" + cliente1.GetSaldo());
                 break;
             case 3:
                 double y;
                 Console.WriteLine("\nQuanto você quer depositar?\n");
                 y = Convert.ToDouble(Console.ReadLine());
-                cliente1.depositar(y);
+                cliente1.Depositar(y);
                 
-                Console.WriteLine("Saldo atual: " + cliente1.GetSaldo());
+                Console.WriteLine("Saldo atual: R$" + cliente1.GetSaldo());
                 break;
                 
             case 4:
