b459fc97b5e6fdf0cd7fcd02a1edaf054adb632c
Roberto Bastos Souza <103335050+RobertoBSZ@users.noreply.github.com>
2023-03-06 21:40:41 -0300

    Add files via upload

--- contabancaria/conta.cs
+++ contabancaria/conta.cs
@@ -1,52 +1,68 @@
 using System;
+using System.Collections.Generic;
 
 class Conta{
     public string Cpf;
     public string Name;
     public double Saldo;
+    public List<string> extrato { get; private set; }
     
     public Conta(){
         this.Cpf = null;
         this.Name = null;
-        this.Saldo = 0;
+        this.Saldo = 1000;
+        extrato = new List<string>();
     }
     
-    public string setCpf{
+    public string GetSetCpf{
         get{
-            return Cpf;
+            return this.Cpf;
         }
         set{
-            Cpf = value;
+            this.Cpf = value;
         }
     }
     
-    public string setName{
+    public string GetSetName{
         get{
-            return Name;
+            return this.Name;
         }
         set{
-            Name = value;
+            this.Name = value;
         }
     }
     
     public double GetSaldo(){
         return this.Saldo;
     }
-    
-    public string GetCpf(){
-        return this.Cpf;
+
+    public void Extrato_Bancario(){
+        foreach (string transacao in extrato){
+            Console.WriteLine(transacao);
+        }
     }
     
-    public string GetName(){
-        return this.Name;
-    }
     public double Sacar (double a){
-        return this.Saldo = Saldo - a;
+        if(Saldo < a){
+            this.extrato.Add("Saque de R$ "+ a + " mal-sucedido");
+            Console.WriteLine("Você possui saldo insuficiente. Saque mal-sucedido.\n");
+            return this.Saldo;
+        }
+        else{
+            this.extrato.Add("Saque de R$ "+ a + " realizado com sucesso!");
+            return this.Saldo = Saldo - a;
+        }
     }
     
     public double Depositar(double a){
-        return this.Saldo = this.Saldo + a;
+        if(a <= 0){
+            this.extrato.Add("Depósito de R$ "+ a +" mal-sucedido");
+            Console.WriteLine("Este depósito é insuficiente. Deposito mal-sucedido.\n");
+            return this.Saldo;
+        }
+        else{
+            this.extrato.Add("Depósito de R$ "+ a +" realizado com sucesso!");
+            return this.Saldo = this.Saldo + a;
+        }
     }
-    
-    
 }
--- contabancaria/main.cs
+++ contabancaria/main.cs
@@ -12,6 +12,8 @@ Ações (Métodos)?
 *Extrato (Pode ser uma string)
 *******************************************************************************/
 using System;
+using System.Collections.Generic;
+
 class HelloWorld {
   static void Main() {
      
@@ -23,24 +25,29 @@ class HelloWorld {
     Console.WriteLine("Criação de Conta\n");
     Console.WriteLine("Cadastro de informações pessoais");
     Console.WriteLine("\nDigite o seu CPF:");
-    cliente1.setCpf = Console.ReadLine();
+    cliente1.GetSetCpf = Console.ReadLine();
     
     
     Console.WriteLine("\nDigite o seu nome:");
-    cliente1.setName = Console.ReadLine();
+    cliente1.GetSetName = Console.ReadLine();
     
     Console.WriteLine("\nConfirmação de suas informações\n");
     Console.WriteLine("CPF: " + cliente1.Cpf);
     Console.WriteLine("Nome: " + cliente1.Name);
     
-    Console.WriteLine("\nOlá " + cliente1.Name + ", Inscrito no CPF" +cliente1.Cpf + ", Seu saldo inicial é de R$1000,00\n");
+    Console.WriteLine("\nOlá " + cliente1.Name + ", inscrito no CPF " + cliente1.Cpf + ", seu saldo inicial é de R$1000,00.\n");
     
     
     while(opcao_menu == true){
-        Console.WriteLine("\nEscolha sua operação bancária:\n1-Consultar\n2-Sacar\n3-Depositar\n4-Sair");
+        Console.WriteLine("\nEscolha sua operação bancária:\n1-Consultar\n2-Sacar\n3-Depositar\n4-Extrato\n5-Sair");
         Console.WriteLine("\nSua escolha:");
         operacaouser = Convert.ToInt32(Console.ReadLine());
-        switch(operacaouser){
+        
+        if(operacaouser <=0 || operacaouser >5){
+            Console.WriteLine("Escolha um número dentre as opções!");
+        }
+        else{
+            switch(operacaouser){
             case 1:
                 Console.WriteLine("\nSaldo atual: R$" + cliente1.GetSaldo());
                 break;
@@ -60,12 +67,18 @@ class HelloWorld {
                 
                 Console.WriteLine("Saldo atual: R$" + cliente1.GetSaldo());
                 break;
-                
+            
             case 4:
+                Console.WriteLine("Transações realizadas:\n");
+                cliente1.Extrato_Bancario();
+                break;
+            case 5:
                 Console.WriteLine("\nVocê escolheu sair. Saindo...");
                 opcao_menu = false;
                 break;
-        }       
+            }       
+        }
+        
     }
   }
 }
