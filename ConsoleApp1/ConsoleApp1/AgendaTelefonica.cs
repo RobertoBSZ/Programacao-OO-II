80461e0fcb3a09ff1bd52a0ed658cbc71cfbf660
Roberto Bastos Souza <103335050+RobertoBSZ@users.noreply.github.com>
2023-03-13 10:47:26 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/AgendaTelefonica.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula230313SistemaAgendaTelefonica
+{
+    public class AgendaTelefonica
+    {
+        public List<Contato> listaAgenda;
+
+        public AgendaTelefonica()
+        {
+            listaAgenda = new List<Contato>();
+        }
+
+        public void inserir(string nome, string numero)
+        {
+            listaAgenda.Add(new Contato(nome, numero));
+        }
+
+        public void remover(string nome)
+        {
+            Contato contato = listaAgenda.Find(c => c.Nome == nome);
+            if (listaAgenda.Remove(Contato))
+            {
+                Console.WriteLine(nome + " foi removido de sua agenda.");
+            }
+            else
+            {
+                Console.WriteLine(nome + " não foi encontradx na sua agenda");
+            }
+        }
+
+        public int tamanhoAgenda()
+        {
+            return listaAgenda.Count();
+        }
+
+        public String buscarNumero(string nome)
+        {
+            String numeroBuscado;
+
+            if (listaAgenda.TryGetValue(nome, out numeroBuscado))
+            {
+                Console.WriteLine("O número de " + nome + " é: " + numeroBuscado);
+            }
+            else
+            {
+                Console.WriteLine(nome + " não foi encontradx na agenda.");
+            }
+            return numeroBuscado;
+        }
+
+        public void imprimirAgenda()
+        {
+
+            Console.WriteLine("\n--------------------------------");
+            foreach (string chave in listaAgenda.Keys)
+            {
+                Console.WriteLine("Nome: " + chave);
+                Console.WriteLine("Telefone: " + listaAgenda[chave]);
+                Console.WriteLine("................");
+            }
+            Console.WriteLine("--------------------------------");
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula230313SistemaAgendaTelefonica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AgendaTelefonica agenda = new AgendaTelefonica();
+
+            agenda.imprimirAgenda();
+
+            agenda.inserir("João", "111111");
+            agenda.inserir("Pedro", "222222");
+            agenda.inserir("Marcos", "333333");
+            agenda.inserir("Matheus", "444444");
+            agenda.inserir("Maria", "555555");
+
+            agenda.imprimirAgenda();
+            Console.WriteLine("\n\nO tamanho é: " + agenda.tamanhoAgenda());
+
+            agenda.remover("Maria");
+
+            agenda.imprimirAgenda();
+            Console.WriteLine("\n\nO tamanho é: " + agenda.tamanhoAgenda());
+
+
+
+        }
+    }
+}
